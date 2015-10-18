b3aafff333894f630de71791709198f8dbfd4da1
CakeBrewery <cakebrewery@gmail.com>
2015-10-17 23:52:17 -0700

    created a style serializer

--- client/app/scripts/services/appstate.js
+++ client/app/scripts/services/appstate.js
@@ -12,20 +12,64 @@ angular.module('clientApp')
     // Service logic
     // ...
 
+
+    /* Serialize JSON into HTML style. 
+    Optional attribute class_name: Return the style inside a css class */
+    var serializeStyle = function(style, class_name){
+      var styleHtml = ""; 
+      if(class_name){
+        styleHtml += class_name + '{';
+      }
+      angular.forEach(style, function(value, key){
+        styleHtml += key + ':' + value + '; '; 
+        console.log(styleHtml); 
+      });
+      if(class_name){
+        styleHtml += '}';
+      }
+
+      return styleHtml; 
+    };
+
     var meaningOfLife = 42;
+
+
+    /* Default Styles */
+    var header_style = {
+      'color': 'white',
+      'height': '50px'
+    };
+
+    var body_style = {}; 
+
+    var footer_style = {
+      'background-color': '#eee',
+      'height': '35px',
+      'width': '100%', 
+      'bottom': '0px',
+      'position' : 'absolute',
+      'text-align':'center',
+      'padding-top': '7px;'
+    };
+
 
     var current_app = "";
 
-    var app_name = "My Site"; 
-    var app_header_style ="color: white; height: 50px;";
-    var app_body_style = "";
-    var app_footer_style="background-color: #eee; height: 35px; width:100%; bottom: 0px; position: absolute; ";
+    var app_name = "";
+    var app_conf = "";
+    var app_header = "";
+    var app_body = "";
+    var app_footer = "";
+    var app_terminator = "";
 
-    var app_conf = "<html><head><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'><title>"+app_name+"</title></head><body>";
-    var app_header = "<div class='userapp-header navbar navbar-inverse' style='"+app_header_style+"'><div class='navbar-brand'>Navbar</div></div>"; 
-    var app_body = "<div class='app_body' style='"+app_body_style+"'><div class='jumbotron'><h1>This is the Body</h1><p>This is the body of the app you are working on</p></div></div>";
-    var app_footer = "<div style='"+app_footer_style+"'><p>Your Footer!</p></div>";
-    var app_terminator = "</body></html>"; 
+    /* Generate Default HTML */
+    app_name="My Site";
+    app_conf = "<html><head><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'><title>"+app_name+"</title></head><body>";
+    app_header = "<div class='navbar navbar-inverse' style='"+ serializeStyle(header_style)+"'><div class='navbar-brand'>Navbar</div></div>"; 
+    app_body = "<div style='"+serializeStyle(body_style)+"'><div class='jumbotron'><h1>This is the Body</h1><p>This is the body of the app you are working on</p></div></div>";
+    app_footer = "<div style='"+serializeStyle(footer_style)+"'><p>Your Footer!</p></div>";
+    app_terminator = "</body></html>"; 
+
 
 
     var appState = {}; 
@@ -48,7 +92,8 @@ angular.module('clientApp')
 
     appState.getCurrentApp = function(){
       var app = {
-        name: current_app,
+        id: current_app,
+        name: app_name,
         conf: app_conf,
         header: app_header,
         body: app_body,
